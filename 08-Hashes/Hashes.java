5f95d0916636277c3b90d75d60817c5edcf1f9af
AitorMonVi <2023_aitor.montejo@iticbcn.cat>
2024-11-21 19:42:09 +0100

    modificado forcaBruta

--- 08-Hashes/Hashes.java
+++ 08-Hashes/Hashes.java
@@ -57,29 +57,29 @@ public class Hashes {
 
     public String forcaBruta(String alg, String hash, String salt) {
         this.npass = 0;
-        final char[] CHARSET = " abcdefABCDEF1234567890!".toCharArray();
+        final char[] CHARSET = "abcdefABCDEF1234567890!".toCharArray();
         char[] password = new char[6];
         // creamos diferentes combinaciones con el CHARSET para poder encontrar la contraseña
         for(int i0 = 0; i0<CHARSET.length; i0++) {
-            password[0]=CHARSET[i0];
             for(int i1 = 0; i1<CHARSET.length; i1++) {
-                password[1]=CHARSET[i1];
                 for(int i2 = 0; i2<CHARSET.length; i2++) {
-                    password[2]=CHARSET[i2];
                     for(int i3 = 0; i3<CHARSET.length; i3++) {
-                        password[3]=CHARSET[i3];
                         for(int i4 = 0; i4<CHARSET.length; i4++) {
-                            password[4]=CHARSET[i4];
                             for(int i5 = 0; i5<CHARSET.length; i5++) {
-                                password[5]=CHARSET[i5];                                                                                                                                
                                 if(tryPassword(password, alg, hash, salt)) {
                                     return new String(password);
                                 }
+                                password[5]=CHARSET[i5];                                                                                                                                
                             }
+                            password[4]=CHARSET[i4];
                         }
+                        password[3]=CHARSET[i3];
                     }
+                    password[2]=CHARSET[i2];
                 }
+                password[1]=CHARSET[i1];
             }   
+            password[0]=CHARSET[i0];
         }
         return null;
     }
